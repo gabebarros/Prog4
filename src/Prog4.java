42f83bd9fb1b8cd631492569a6d82933c7d06558
gabebarros <gabe.barros@icloud.com>
2025-05-03 13:29:06 -0700

    added update member

--- src/Prog4.java
+++ src/Prog4.java
@@ -96,7 +96,87 @@ public class Prog4 {
             stmt = dbconn.createStatement();
 		    answer = stmt.executeUpdate(query);
 	            
-            System.out.println("member inserted");
+            System.out.println("Member inserted");
+		
+		        // Shut down the connection to the DBMS.
+		
+		    stmt.close();  
+		    dbconn.close();
+		
+		} catch (SQLException e) {
+		
+		        System.err.println("*** SQLException:  "
+		            + "Could not fetch query results.");
+		        System.err.println("\tMessage:   " + e.getMessage());
+		        System.err.println("\tSQLState:  " + e.getSQLState());
+		        System.err.println("\tErrorCode: " + e.getErrorCode());
+		        System.exit(-1);
+		
+		}
+		
+	}
+	
+	private static void updateMember(String email, String category, String change) {
+		
+		final String oracleURL =   // Magic lectura -> aloe access spell
+                "jdbc:oracle:thin:@aloe.cs.arizona.edu:1521:oracle";
+	
+		
+		String username = "gabebarros",    // Oracle DBMS username
+		       password = "a7693";    // Oracle DBMS password
+		
+		    // load the (Oracle) JDBC driver by initializing its base
+		    // class, 'oracle.jdbc.OracleDriver'.
+		
+		try {
+		
+		        Class.forName("oracle.jdbc.OracleDriver");
+		
+		} catch (ClassNotFoundException e) {
+		
+		        System.err.println("*** ClassNotFoundException:  "
+		            + "Error loading Oracle JDBC driver.  \n"
+		            + "\tPerhaps the driver is not on the Classpath?");
+		        System.exit(-1);
+		
+		}
+		
+		    // make and return a database connection to the user's
+		    // Oracle database
+		
+		Connection dbconn = null;
+		
+		try {
+		        dbconn = DriverManager.getConnection
+		                       (oracleURL,username,password);
+		
+		} catch (SQLException e) {
+		
+		        System.err.println("*** SQLException:  "
+		            + "Could not open JDBC connection.");
+		        System.err.println("\tMessage:   " + e.getMessage());
+		        System.err.println("\tSQLState:  " + e.getSQLState());
+		        System.err.println("\tErrorCode: " + e.getErrorCode());
+		        System.exit(-1);
+		
+		}
+		
+		    // Send the query to the DBMS, and get and display the results
+		
+		Statement stmt = null;
+		int answer;
+		int mid = 0;
+		
+		try { 
+            String query =       // our test query
+            		"UPDATE bhousmans.member" 
+            		+ " SET " + category + "='" + change + "'"
+            		+ " WHERE email='" + email + "'";
+
+            stmt = dbconn.createStatement();
+		    answer = stmt.executeUpdate(query);
+	            
+            System.out.println("Member updated");
 		
 		        // Shut down the connection to the DBMS.
 		
@@ -121,7 +201,8 @@ public class Prog4 {
 
         // the string to display to users when they run the program
         String menuStr = "Options: \n"
-				+ "1 - Add Member";
+				+ "1 - Add Member \n"
+        		+ "2 - Update Member";
 				
 		// prompt for operations/queries until termination
         while (true) {
@@ -158,6 +239,22 @@ public class Prog4 {
                String emergency = scanner.nextLine();  // store emergency
                
                addMember(fname, lname, email, dob, phone, emergency, 1);
+               
+           }
+           else if (input.strip().equals("2")) {
+        	   System.out.println("What is the member's email?");
+        	   System.out.println();
+               String email = scanner.nextLine();  // store email
+               
+               System.out.println("What would you like to update? (phone, email, emergency)");
+        	   System.out.println();
+               String category = scanner.nextLine();  // store category
+               
+               System.out.println("What would you like to change it to?");
+        	   System.out.println();
+               String change = scanner.nextLine();  // store change
+               
+               updateMember(email, category, change);
                
            }
            else {
