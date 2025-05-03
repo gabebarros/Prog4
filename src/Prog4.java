40efd8878c1ea7d88a6585503feab24a955afd8e
gabebarros <gabe.barros@icloud.com>
2025-05-03 14:55:44 -0700

    added delete member

--- src/Prog4.java
+++ src/Prog4.java
@@ -165,7 +165,6 @@ public class Prog4 {
 		
 		Statement stmt = null;
 		int answer;
-		int mid = 0;
 		
 		try { 
             String query =       // our test query
@@ -196,13 +195,267 @@ public class Prog4 {
 		
 	}
 	
+private static void deleteMember(String email) {
+		
+		final String oracleURL =   // Magic lectura -> aloe access spell
+                "jdbc:oracle:thin:@aloe.cs.arizona.edu:1521:oracle";
+	
+		
+		String username = "gabebarros",    // Oracle DBMS username
+		       password = "a7693";    // Oracle DBMS password
+		
+		    // load the (Oracle) JDBC driver by initializing its base
+		    // class, 'oracle.jdbc.OracleDriver'.
+		
+		try {
+		
+		        Class.forName("oracle.jdbc.OracleDriver");
+		
+		} catch (ClassNotFoundException e) {
+		
+		        System.err.println("*** ClassNotFoundException:  "
+		            + "Error loading Oracle JDBC driver.  \n"
+		            + "\tPerhaps the driver is not on the Classpath?");
+		        System.exit(-1);
+		
+		}
+		
+		    // make and return a database connection to the user's
+		    // Oracle database
+		
+		Connection dbconn = null;
+		
+		try {
+		        dbconn = DriverManager.getConnection
+		                       (oracleURL,username,password);
+		
+		} catch (SQLException e) {
+		
+		        System.err.println("*** SQLException:  "
+		            + "Could not open JDBC connection.");
+		        System.err.println("\tMessage:   " + e.getMessage());
+		        System.err.println("\tSQLState:  " + e.getSQLState());
+		        System.err.println("\tErrorCode: " + e.getErrorCode());
+		        System.exit(-1);
+		
+		}
+		
+		    // Send the query to the DBMS, and get and display the results
+		
+		Statement stmt = null;
+		int answer;
+		int mid = getMidByEmail(email);
+		
+		if (!memberCanBeDeleted(mid)) {
+			System.out.println("Member cannot be deleted because there are active ski passes, rental records, or lesson sessions."
+					+ " Please complete or cancel these before deleting your membership.");
+			return;
+		}
+		
+		try { 
+            String query =       // our test query
+            		"DELETE FROM bhousmans.member" 
+            		+ " WHERE mid=" + mid;
+
+            stmt = dbconn.createStatement();
+		    answer = stmt.executeUpdate(query);
+		    	            
+            System.out.println("Member deleted");
+		
+		        // Shut down the connection to the DBMS.
+		
+		    stmt.close();  
+		    dbconn.close();
+		
+		} catch (SQLException e) {
+		
+		        System.err.println("*** SQLException:  "
+		            + "Could not fetch query results.");
+		        System.err.println("\tMessage:   " + e.getMessage());
+		        System.err.println("\tSQLState:  " + e.getSQLState());
+		        System.err.println("\tErrorCode: " + e.getErrorCode());
+		        System.exit(-1);
+		
+		}
+		
+	}
+
+	private static int getMidByEmail(String email) {
+		
+		final String oracleURL =   // Magic lectura -> aloe access spell
+	            "jdbc:oracle:thin:@aloe.cs.arizona.edu:1521:oracle";
+	
+		String username = "gabebarros",    // Oracle DBMS username
+		       password = "a7693";    // Oracle DBMS password
+		
+		    // load the (Oracle) JDBC driver by initializing its base
+		    // class, 'oracle.jdbc.OracleDriver'.
+		
+		try {
+		
+		        Class.forName("oracle.jdbc.OracleDriver");
+		
+		} catch (ClassNotFoundException e) {
+		
+		        System.err.println("*** ClassNotFoundException:  "
+		            + "Error loading Oracle JDBC driver.  \n"
+		            + "\tPerhaps the driver is not on the Classpath?");
+		        System.exit(-1);
+		
+		}
+		
+		    // make and return a database connection to the user's
+		    // Oracle database
+		
+		Connection dbconn = null;
+		
+		try {
+		        dbconn = DriverManager.getConnection
+		                       (oracleURL,username,password);
+		
+		} catch (SQLException e) {
+		
+		        System.err.println("*** SQLException:  "
+		            + "Could not open JDBC connection.");
+		        System.err.println("\tMessage:   " + e.getMessage());
+		        System.err.println("\tSQLState:  " + e.getSQLState());
+		        System.err.println("\tErrorCode: " + e.getErrorCode());
+		        System.exit(-1);
+		
+		}
+		
+		    // Send the query to the DBMS, and get and display the results
+		
+		Statement stmt = null;
+		int mid = -1;
+		
+		try { 
+	        String query =       // our test query
+	        		"SELECT mid FROM bhousmans.member" 
+	        		+ " WHERE email='" + email + "'";
+	
+	        stmt = dbconn.createStatement();
+	        ResultSet rs = stmt.executeQuery(query);
+	        
+	        if (rs.next()) {
+	            mid = rs.getInt("mid");
+	        }
+		
+		        // Shut down the connection to the DBMS.
+		
+	        rs.close();
+		    stmt.close();  
+		    dbconn.close();
+		    
+		
+		} catch (SQLException e) {
+		
+		        System.err.println("*** SQLException:  "
+		            + "Could not fetch query results.");
+		        System.err.println("\tMessage:   " + e.getMessage());
+		        System.err.println("\tSQLState:  " + e.getSQLState());
+		        System.err.println("\tErrorCode: " + e.getErrorCode());
+		        System.exit(-1);
+		
+		}
+		return mid;
+		
+	}
+
+private static boolean memberCanBeDeleted(int mid) {
+		
+		final String oracleURL =   // Magic lectura -> aloe access spell
+	            "jdbc:oracle:thin:@aloe.cs.arizona.edu:1521:oracle";
+	
+		String username = "gabebarros",    // Oracle DBMS username
+		       password = "a7693";    // Oracle DBMS password
+		
+		    // load the (Oracle) JDBC driver by initializing its base
+		    // class, 'oracle.jdbc.OracleDriver'.
+		
+		try {
+		
+		        Class.forName("oracle.jdbc.OracleDriver");
+		
+		} catch (ClassNotFoundException e) {
+		
+		        System.err.println("*** ClassNotFoundException:  "
+		            + "Error loading Oracle JDBC driver.  \n"
+		            + "\tPerhaps the driver is not on the Classpath?");
+		        System.exit(-1);
+		
+		}
+		
+		    // make and return a database connection to the user's
+		    // Oracle database
+		
+		Connection dbconn = null;
+		
+		try {
+		        dbconn = DriverManager.getConnection
+		                       (oracleURL,username,password);
+		
+		} catch (SQLException e) {
+		
+		        System.err.println("*** SQLException:  "
+		            + "Could not open JDBC connection.");
+		        System.err.println("\tMessage:   " + e.getMessage());
+		        System.err.println("\tSQLState:  " + e.getSQLState());
+		        System.err.println("\tErrorCode: " + e.getErrorCode());
+		        System.exit(-1);
+		
+		}
+		
+		    // Send the query to the DBMS, and get and display the results
+		
+		Statement stmt = null;
+		
+		try { 
+			String query =   // check if there are any tuples
+				    "SELECT mid FROM bhousmans.skipass WHERE mid = " + mid + 
+				    " UNION " +
+				    "SELECT mid FROM bhousmans.lessonreg WHERE mid = " + mid;
+	
+	        stmt = dbconn.createStatement();
+	        ResultSet rs = stmt.executeQuery(query);
+	        
+	        // return false if there are any tuples
+	        if (rs.next()) {
+	            return false;
+	        }
+		
+		        // Shut down the connection to the DBMS.
+		
+	        rs.close();
+		    stmt.close();  
+		    dbconn.close();
+		    
+		
+		} catch (SQLException e) {
+		
+		        System.err.println("*** SQLException:  "
+		            + "Could not fetch query results.");
+		        System.err.println("\tMessage:   " + e.getMessage());
+		        System.err.println("\tSQLState:  " + e.getSQLState());
+		        System.err.println("\tErrorCode: " + e.getErrorCode());
+		        System.exit(-1);
+		
+		}
+		
+		return true;
+		
+	}
+	
+	
+	
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);  // use to get user input
 
         // the string to display to users when they run the program
         String menuStr = "Options: \n"
 				+ "1 - Add Member \n"
-        		+ "2 - Update Member";
+        		+ "2 - Update Member \n"
+        		+ "3 - Delete Member";
 				
 		// prompt for operations/queries until termination
         while (true) {
@@ -257,6 +510,13 @@ public class Prog4 {
                updateMember(email, category, change);
                
            }
+           else if (input.strip().equals("3")) {
+        	   System.out.println("What is the member's email?");
+        	   System.out.println();
+               String email = scanner.nextLine();  // store email
+               
+               deleteMember(email);
+           }
            else {
         	   System.out.println("Invalid option");
         	   System.out.println();
@@ -267,5 +527,4 @@ public class Prog4 {
        scanner.close();
 
 	}
-
 }
